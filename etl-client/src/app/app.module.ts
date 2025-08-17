7f94aced54943384cf53ffeffcde10fa3a9b1f40
Arash Zarghami <eng.ar.zarghami@gmail.com>
2025-08-17 15:58:50 +0330

    config primeng

--- etl-client/src/app/app.module.ts
+++ etl-client/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import {ButtonDemoComponent} from '../button-demo/button-demo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,],
   bootstrap: [AppComponent],
-  imports: [BrowserModule]
+  imports: [BrowserModule, ButtonDemoComponent],
+  providers: [provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })]
 })
 export class AppModule {
 }
