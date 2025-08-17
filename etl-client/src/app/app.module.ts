ae15e60b1e7790aa3f1ad4115c98d3e1ed9e7a4a
Arash Zarghami <eng.ar.zarghami@gmail.com>
2025-08-17 14:24:40 +0330

    write keycloack

--- etl-client/src/app/app.module.ts
+++ etl-client/src/app/app.module.ts
@@ -1,11 +1,35 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
+import {NgModule, APP_INITIALIZER} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        // this is the url shoud devops give use
+        url: 'http://localhost:8000/',
+        realm: 'myrealm',
+        clientId: 'my-angular-client',
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: true,
+      },
+    });
+}
 
 @NgModule({
   declarations: [AppComponent],
+  imports: [BrowserModule, KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      deps: [KeycloakService],
+    },
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule]
 })
 export class AppModule {
 }
