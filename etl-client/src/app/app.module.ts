95ff7fd32702e3ea5c0e11b1cb9107ea15866016
Masih Abedini <masih7816@gmail.com>
2025-08-18 10:28:16 +0330

    fix: remove redundant codes

--- etl-client/src/app/app.module.ts
+++ etl-client/src/app/app.module.ts
@@ -1,26 +1,9 @@
-import {APP_INITIALIZER, NgModule} from "@angular/core";
+import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from './app.component';
-import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-
-function initializeKeycloak(keycloak: KeycloakService) {
-  return () =>
-    keycloak.init({
-      config: {
-        // this is the url shoud devops give use
-        url: 'http://localhost:8000/',
-        realm: 'myrealm',
-        clientId: 'my-angular-client',
-      },
-      initOptions: {
-        onLoad: 'login-required',
-        checkLoginIframe: true,
-      },
-    });
-}
 
 
 @NgModule({
@@ -32,11 +15,7 @@ function initializeKeycloak(keycloak: KeycloakService) {
       theme: {
         preset: Aura
       }
-    }), {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      deps: [KeycloakService],
-    },]
+    })]
 })
 export class AppModule {
 }
