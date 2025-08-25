d03c10bf102e1fe7b5c7f62c84dfce8eb9da032b
Zahra Ab <zahraboootalebi@gmail.com>
2025-08-25 13:20:06 +0330

    infrastructure auth tests added

---
+++ etl-server/tests/ETL.Infrastructure.Tests/OAuth/Fixture/HttpClientCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace ETL.Infrastructure.Tests.OAuth.Fixture;
+
+[CollectionDefinition("HttpClient collection")]
+public class HttpClientCollection : ICollectionFixture<HttpClientTestFixture>
+{
+    
+}
--- etl-server/tests/ETL.Infrastructure.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace ETL.Infrastructure.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
