9148d0ed59856ffbb6eab31910a52ce90996f71a
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-08-26 16:42:14 +0330

    CORS policy added.

--- etl-server/src/ETL.API/Program.cs
+++ etl-server/src/ETL.API/Program.cs
@@ -6,40 +6,45 @@ using ETL.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddLogging();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Angular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
-
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
 app.UseExceptionHandler();
-
 app.UseHttpsRedirection();
 
-app.UseTokenRefresh();
 
+app.UseCors("Angular");
+
+app.UseTokenRefresh();
 app.UseAuthentication();
 app.UseAuthorization();
-
-
 
 app.MapControllers();
 
